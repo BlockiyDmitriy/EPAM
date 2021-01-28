00012e434b46b25d9c9f30b0877831a1077890cc
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-01-28 21:26:12 +0300

    creating CSVParsing
    need to improve parsing in GeEnumirator()

---
+++ Task4/Task4.BLL/CSVParsing/CSVDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.BLL.CSVParsing
+{
+    public class CSVDTO
+    {
+        public string DateTime { get; set; }
+        public string ClientName { get; set; }
+        public string Product { get; set; }
+        public string Sum { get; set; }
+    }
+}
