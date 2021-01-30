0282a2a066a3f69f8afe799ff61b99fa59bc0fb5
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-01-30 22:45:16 +0300

    add configuration
    add source to folder with csv files

---
+++ Task4/Task4.BLL/Managers/ParseFileServiceTaskManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data.Entity;
+using System.IO;
+using System.Threading.Tasks;
+using System.Transactions;
+using Task4.BLL.Abstraction;
+using Task4.BLL.CSVParsing;
+using Task4.DAL.Context;
+
+namespace Task4.BLL.Managers
+{
+    public class ParseFileServiceTaskManager : IControlProcess
+    {
+        private IFileManager FileManager { get; set; }
+        private TaskFactory TaskFactory { get; set; } = new TaskFactory();
+        private TaskManager TaskManager { get; set; } = new TaskManager();
+
+        Action<object> ParsingAction = param =>
+        {
+            string fileName = param as string;
+
+            using (var DTOSource = new StringToDTOParser(new StreamReader(fileName)))
+            {
+                foreach (var item in DTOSource)
+                {
+                    TransactionScope scope = null;
+                    DbContext context = null;
+                    try
+                    {
+                        context = new FileDataModelContainer();
+                        //scope = 
+                    }
+                    finally
+                    {
+                        scope.Dispose();
+                        context.Dispose();
+                        GC.SuppressFinalize(scope);
+                        GC.SuppressFinalize(context);
+                    }
+                }
+            }
+        };
+
+        public void RunTask(string fileName)
+        {
+            Task temp;
+            try
+            {
+                TaskManager.TryTakeTask(temp = TaskFactory.StartNew(ParsingAction, fileName, TaskFactory.CancellationToken));
+                temp.ContinueWith(x =>
+                {
+                    FileManager.BackUp(fileName);
+
+                });
+            }
+            catch
+            {
+
+            }
+        }
+        public void Start()
+        {
+           
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
