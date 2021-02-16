f65773da359ca3da17366a4d0b5b74e1ed92e7c1
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-02-16 20:53:59 +0300

    Add role (user, administrator)

---
+++ Task5/Task5.WebClient/Models/AppDbInitializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace Task5.WebClient.Models
+{
+    public class AppDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            // создаем две роли
+            var roleAdmin = new IdentityRole { Name = "admin" };
+            var roleUser = new IdentityRole { Name = "user" };
+
+            // добавляем роли в бд
+            roleManager.Create(roleAdmin);
+            roleManager.Create(roleUser);
+
+            // создаем пользователей
+            var admin = new ApplicationUser { Email = "dimiyan01@gmail.com", UserName = "Dima" };
+            string password = "ad46D_ewr3";
+            var result = userManager.Create(admin, password);
+
+            // если создание пользователя прошло успешно
+            if (result.Succeeded)
+            {
+                // добавляем для пользователя роль
+                userManager.AddToRole(admin.Id, roleAdmin.Name);
+                userManager.AddToRole(admin.Id, roleUser.Name);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
