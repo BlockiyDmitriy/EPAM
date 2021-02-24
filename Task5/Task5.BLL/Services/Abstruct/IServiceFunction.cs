dfce906c0d6297f2361c0b3e29b930c7d6d19cf9
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-02-24 23:37:12 +0300

    Create IServiceFunction.cs

---
+++ Task5/Task5.BLL/Services/Abstruct/IServiceFunction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.BLL.Services.Abstruct
+{
+    public interface IServiceFunction<TDTO> where TDTO : class
+    {
+        void Create(TDTO tDTO);
+        void Update(TDTO tDTO);
+        void Remove(TDTO tDTO);
+    }
+}
