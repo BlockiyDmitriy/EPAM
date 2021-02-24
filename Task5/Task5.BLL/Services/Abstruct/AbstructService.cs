21b10d764c442a304644c349e78986b935a57c08
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-02-24 23:34:21 +0300

    Create AbstructService.cs

---
+++ Task5/Task5.BLL/Services/Abstruct/AbstructService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper.QueryableExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Task5.BLL.Extentions;
+using Task5.DAL.Repositories;
+using Task5.DAL.UoW;
+
+namespace Task5.BLL.Services.Abstruct
+{
+    public abstract class AbstructService
+    {
+        protected UnitOfWork UOW { get; set; }
+
+        private bool disposed = false;
+        protected AbstructService(UnitOfWork uOW)
+        {
+            this.UOW = uOW;
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    UOW.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+    }
+}
