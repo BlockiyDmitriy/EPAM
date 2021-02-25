c14f2aa00b799dc09034084c79e7ca8d2d86d2bf
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-02-25 11:36:55 +0300

    Create IServiceGetFunction.cs

---
+++ Task5/Task5.BLL/Services/Abstruct/IServiceGetFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.BLL.Services.Abstruct
+{
+    public interface IServiceGetFunction<TDTO> where TDTO : class
+    {
+        IEnumerable<TDTO> Get();
+
+        IEnumerable<TDTO> Get(Expression<Func<TDTO, bool>> predicate);
+        TDTO SingleOrDefault(Expression<Func<TDTO, bool>> predicate);
+    }
+}
