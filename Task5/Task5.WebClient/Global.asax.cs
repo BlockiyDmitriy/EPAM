f3a5278bea3b93658946833ca6b66cd983bb1136
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-02-25 11:23:29 +0300

    Update Global.asax.cs

--- Task5/Task5.WebClient/Global.asax.cs
+++ Task5/Task5.WebClient/Global.asax.cs
@@ -1,3 +1,6 @@
+using Ninject;
+using Ninject.Modules;
+using Ninject.Web.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -6,6 +9,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Task5.BLL.Extentions;
 using Task5.WebClient.Extensions;
 using Task5.WebClient.Models;
 
@@ -21,7 +25,11 @@ namespace Task5.WebClient
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            MapperHelper.MapperConfig();
+            BLL.Extentions.MapperHelper.MapperConfig();
+
+            NinjectModule registrations = new NinjectRegistrations();
+            var kernel = new StandardKernel(registrations);
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }
     }
 }
