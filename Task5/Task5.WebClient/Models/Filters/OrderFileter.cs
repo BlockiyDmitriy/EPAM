3192cb10d533a4085ade2d121673fa5c3242f046
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-03-03 18:55:16 +0300

    Create OrderFileter.cs

---
+++ Task5/Task5.WebClient/Models/Filters/OrderFileter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Task5.WebClient.Models.Filters
+{
+    public class OrderFileter
+    {
+        [DataType(DataType.Date)]
+        [Display(Name ="Date")]
+        public DateTime DateTime { get; set; }
+        [Display(Name ="Name")]
+        public string ClientName { get; set; }
+        [Display(Name ="Product")]
+        public string ProductName { get; set; }
+    }
+}
