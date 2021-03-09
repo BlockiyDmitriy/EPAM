a449f1d521535459eafcbcaee6c6bd611bc4f3e2
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-03-09 09:03:12 +0300

    Create ClientFilter.cs

---
+++ Task5/Task5.WebClient/Models/Filters/ClientFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Task5.WebClient.Models.Filters
+{
+    public class ClientFilter
+    {
+        [Display(Name ="Name")]
+        public string Name { get; set; }
+    }
+}
